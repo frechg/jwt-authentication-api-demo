1229a8324b7fa4123286b6687f381925412a62f1
frechg <galenfrechette@gmail.com>
2022-05-27 20:34:11 +0700

    Adds MethodOverride and Flash middleware.

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module JwtAuthenticationApiDemo
 
     config.api_only = true
 
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
